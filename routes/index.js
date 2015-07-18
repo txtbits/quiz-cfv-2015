9b9b1b5af00b4287e99033cd4d005e78e3f0b69d
Christian Forcada Vitalla <chris.forcadavitalla@gmail.com>
2015-07-18 20:36:06 +0200

    Final

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ router.get('/', function(req, res) {
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 router.get('/author', function(req, res){
-	res.render('author', { title: 'Quiz 2015' });
+	res.render('author');
 });
 
 module.exports = router;

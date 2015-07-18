b360cbfa161e40fec6416e017b255a09ede88b9f
Christian Forcada Vitalla <chris.forcadavitalla@gmail.com>
2015-07-18 20:30:10 +0200

    Últimos cambios

--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,13 @@ var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz1' });
+  res.render('index', { title: 'Quiz 2015' });
 });
 
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
-
-router.get('/author', function(req, res) {
-  res.render('/author', /views/author.ejs);
+router.get('/author', function(req, res){
+	res.render('author');
 });
 
 module.exports = router;
